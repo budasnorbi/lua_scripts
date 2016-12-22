54c77ce166f402b9b785fbe8f939bbc3905a5a87
Budás Norbert <budasnorbert@gmail.com>
2016-12-23 00:40:31 +0100

    Paladin Duel Ai some changes
    -messages object
    -optimized healing event
    -fixed lower than 10% hp bugs
    -fixed buffing multiple times
    -new enter combat message
    -new leavecombat sayText and yell on player

--- lvl10_Paladin_Duel.lua
+++ lvl10_Paladin_Duel.lua
@@ -8,16 +8,23 @@ local BLESSING_OF_MIGHT = 19740
 local JUDGEMENT_OF_LIGHT = 20271
 local ProtectionSpells = {498,1022,633}
 
+local messages = {
+	"You have been slained",
+	"STAGE 1 FAILED",
+	"You are reached my loot",
+	"Stage 1 success",
+	"STAGE 1 STARTED"
+}
+
 function HammerOfJustice(event, delay, repeats, creature)
 	local Victim = creature:GetVictim();
-	if(Victim:IsCasting())then
+	if( Victim:IsCasting())then
 		creature:CastSpell(Victim, HAMMER_OF_JUSTICE, false)
 	end
 end
 
-
 function HolyLight(event, delay, repeats, creature)
-	if ( math.random(1, 100) >= 65 and creature:HealthPct() < 55 and not creature:IsCasting()) then
+	if ( creature:GetHealthPct() < 45 and math.random(1, 100) >= 75 and not creature:IsCasting()) then
 		creature:CastSpell(creature, HOLY_LIGHT_RANK_2, false)
 	end
 end
@@ -25,9 +32,10 @@ end
 function RandomSpell(event, delay, repeats, creature)
 	local HealthPct = creature:GetHealthPct()
 	local ObjKey = math.random(1,3)
-	if HealthPct <= 10 then
+
+	if (HealthPct <= 10 and not creature:HasAura(25771)) then
 		creature:CastSpell(creature, ProtectionSpells[ObjKey], true)
-		if ProtectionSpells ~= 633 then
+		if (ProtectionSpells ~= 633 ) then
 			creature:CastSpell(creature, HOLY_LIGHT_RANK_2, false)
 		end
 	end
@@ -39,17 +47,27 @@ function JudgementOfLight(event, delay, repeats, creature)
 end
 
 function AddAura(event, delay, repeats, creature)
-	creature:CastSpell(creature,SEAL_OF_RIGHTEOUSNESS, true)
+	creature:CastSpell(creature,SEAL_OF_RIGHTEOUSNESS, false)
 	creature:CastSpell(creature,BLESSING_OF_MIGHT, true)
 	creature:CastSpell(creature,DEVOTION_AURA, true)
 end
 
+function openText(event, delay, repeats, creature)
+	creature:SendUnitSay(messages[5],0)
+end
+
+function KilledByCreature(event, killer, killed)
+	killer:SendUnitSay(messages[1],0)
+end
+
 function OnEnterCombat(event,creature)
+	creature:RegisterEvent(openText,1000,1)
 	creature:RegisterEvent(HolyLight,6000,0)
 	creature:RegisterEvent(RandomSpell,2500,0)
-	creature:RegisterEvent(AddAura,1000,2)
+	creature:RegisterEvent(AddAura,1000,1)
 	creature:RegisterEvent(JudgementOfLight,9000,0)
-	creature:RegisterEvent(HammerOfJustice,1000,0)
+	creature:RegisterEvent(HammerOfJustice,2500,0)
+
 end
 
 function OnLeaveCombat(event,creature)
@@ -57,9 +75,12 @@ function OnLeaveCombat(event,creature)
 end
 
 function OnDied(event, creature, killer)
+ 	killer:SendUnitSay(messages[4],0)
+ 	killer:Emote(21)
  	creature:RemoveEvents();
 end
 
 RegisterCreatureEvent(NPC_ID,1,OnEnterCombat);
 RegisterCreatureEvent(NPC_ID,2,OnLeaveCombat);
 RegisterCreatureEvent(NPC_ID,4,OnDied)
+RegisterPlayerEvent(34,KilledByCreature)
