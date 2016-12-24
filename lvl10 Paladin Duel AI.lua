e54121258f63c91a460309b0d5707ed695c38192
Budás Norbert <budasnorber@gmail.com>
2016-12-24 14:03:55 +0100

    Commenting functions . Minor Fixes.

--- lvl10 Paladin Duel AI.lua
+++ lvl10 Paladin Duel AI.lua
@@ -6,14 +6,23 @@ local HAMMER_OF_JUSTICE = 853
 local BLESSING_OF_MIGHT = 19740
 local JUDGEMENT_OF_LIGHT = 20271
 local ProtectionSpells = {498,1022,633}
+local texts = {
+	"Now , you can loot my treasures",
+	"We meet each other at level 20",
+	"The duel is started"
+}
 
---Timer Function
+--Timer Function: can use inside every function . 
 function wait(seconds)
   local start = os.time()
   repeat until os.time() > start + seconds
 end
 
---Spell Functions
+
+-- SPELL TRIGGERS
+
+--Hammer Of Justice: Only cast this spell if the victim is casting , but creature isn't casting.
+--Bonus Funtion: If creature isn't cooldown on Judgement Of Light , it can combine these spell and combo then.
 function HammerOfJustice(event, delay, repeats, creature)
 	local Victim = creature:GetVictim()
 	if (Victim:IsCasting() and not creature:IsCasting()) then
@@ -24,12 +33,16 @@ function HammerOfJustice(event, delay, repeats, creature)
 	end
 end
 
+--Holy Light : Only cast this spell , if the creature healt is lower than 50 and the random number is bigger or equal than 65
 function HolyLight(event, delay, repeats, creature)
 	if (creature:GetHealthPct() < 50 and math.random(1, 100) >= 65 ) then
 		creature:CastSpell(creature, HOLY_LIGHT_RANK_2, false)
 	end
 end
 
+--Only use these spell if the creature health is under or equal than 25 and isn't has aura.
+--Bug fix : If the create has aura , in this case doesn't cast ProtectionSpells .
+--If the spell number is 3 from the generated random numbers , then Holy Light doesn't cast.
 function ProtectionLastPhase(event, delay, repeats, creature)
 	local ObjKey = math.random(1,3)
 	if (creature:GetHealthPct() <= 25 and not creature:HasAura(25771)) then
@@ -40,6 +53,8 @@ function ProtectionLastPhase(event, delay, repeats, creature)
 	end
 end
 
+--JudgementOfLight: Cast victim target spell
+--Bugfix:If the creature doesn't casting , this spell only will be use.
 function JudgementOfLight(event, delay, repeats, creature)
 	local Victim = creature:GetVictim();
 	if(not creature:IsCasting())then
@@ -47,35 +62,39 @@ function JudgementOfLight(event, delay, repeats, creature)
 	end
 end
 
+--Add Auras 
 function AddAura(event, delay, repeats, creature)
 	creature:CastSpell(creature,SEAL_OF_RIGHTEOUSNESS, false)
 	creature:CastSpell(creature,BLESSING_OF_MIGHT, true)
 	creature:CastSpell(creature,DEVOTION_AURA, true)
 end
 
+--When the creature is going to death . These texts will be displayed.
 function CombatDeadText(event, delay, repeats, creature)
 	wait(1)
-	creature:SendUnitSay("Now , you can loot my treasures",0)
+	creature:SendUnitSay(texts[1],0)
 	wait(1)
-	creature:SendUnitSay("We meet each other at level 20",0)
+	creature:SendUnitSay(texts[2],0)
 end
 
---Gossip
+--Gossip function
 function Gossip(event, player, unit)
 	player:GossipClearMenu()
     player:GossipMenuAddItem(0, "Are you ready for the duel ?", 0, 1)
     player:GossipSendMenu(1, unit)
 end
 
+--When the player selected this gossip item . These function will be activated .
 function OnGossipSelect(event, player, unit, sender, intid, code)
 	if(intid == 1) then
+		player:GossipComplete()
         unit:SendUnitSay("3",0)
         wait(1)
 		unit:SendUnitSay("2",0)
 		wait(1)
 		unit:SendUnitSay("1",0)
 		wait(1)
-		unit:SendUnitSay("The duel is started!",0)
+		unit:SendUnitSay(texts[3],0)
         unit:SetNPCFlags(0)
         unit:SetFaction(14)
         unit:AttackStart(player)
@@ -103,9 +122,11 @@ function OnDied(event,creature,killer)
 	creature:RemoveEvents()
 end
 
-RegisterCreatureGossipEvent(NPC_ID, 1, Gossip)
-RegisterCreatureGossipEvent(NPC_ID, 2, OnGossipSelect)
-
+--Creature Events
 RegisterCreatureEvent(NPC_ID,1,OnEnterCombat);
 RegisterCreatureEvent(NPC_ID,2,OnLeaveCombat);
 RegisterCreatureEvent(NPC_ID,4,OnDied)
+
+--Create Gossip Events
+RegisterCreatureGossipEvent(NPC_ID, 1, Gossip)
+RegisterCreatureGossipEvent(NPC_ID, 2, OnGossipSelect)
